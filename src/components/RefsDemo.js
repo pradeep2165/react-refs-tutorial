1db2ce7706334689be3e33f2f420864c7d8fd12a
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-15 20:58:38 +0530

    refs used in click event to alert value of input

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -9,10 +9,14 @@ class RefsDemo extends Component {
     this.inputRef.current.focus();
     console.log(this.inputRef);
   }
+  clickHandler = () => {
+    alert(this.inputRef.current.value);
+  };
   render() {
     return (
       <div>
         <input ref={this.inputRef} type="text" />
+        <button onClick={this.clickHandler}>Click</button>
       </div>
     );
   }
