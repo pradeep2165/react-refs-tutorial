05af442cbf3597ec84e01c85f3f6a163e3dec517
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-15 23:22:45 +0530

    refs used in class component to focus in child component

---
+++ src/components/FocusInput.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Input from "./Input";
+
+class FocusInput extends Component {
+  constructor() {
+    super();
+    this.componentRef = React.createRef();
+  }
+
+  clickHandler = () => {
+    this.componentRef.current.focusInput();
+  };
+  render() {
+    return (
+      <div>
+        <Input ref={this.componentRef} />
+        {/* <input type="text" ref={this.componentRef} /> */}
+        <button onClick={this.clickHandler}>Focus Input</button>
+      </div>
+    );
+  }
+}
+
+export default FocusInput;
